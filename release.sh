28aca7556593642bebed06d491890e581bbbaf28
mehdi.el-kouhen-ext@vif.fr-at-977960131096 <mehdi.el-kouhen-ext@vif.fr>
2018-07-18 22:12:39 +0200

    commit

--- release.sh
+++ release.sh
@@ -1,9 +1,10 @@
-version=0.1.11
+version=0.1.12
 
-helm package --version $version api-gateway
+#helm package --version $version api-gateway
 #helm package --version $version evenement-parcours-integration
 #helm package --version $version evenement-rappel
 #helm package --version $version gestion-evenement
+helm package --version $version gestion-images
 #helm package --version $version gestion-personnes
 #helm package --version $version parcours-integration
 #helm package --version $version referentiel-personnes-api
