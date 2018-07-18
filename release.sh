08f77ed59930e3dd31e0cf50cc2cdcb741604299
mehdi.el-kouhen-ext@vif.fr-at-977960131096 <mehdi.el-kouhen-ext@vif.fr>
2018-07-18 22:30:22 +0200

    commit

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
-version=0.1.13
+version=0.1.14
 
 #helm package --version $version api-gateway
 #helm package --version $version evenement-parcours-integration
 #helm package --version $version evenement-rappel
 #helm package --version $version gestion-evenement
-helm package --version $version gestion-images
-#helm package --version $version gestion-personnes
-#helm package --version $version parcours-integration
+#helm package --version $version gestion-images
+helm package --version $version gestion-personnes
+helm package --version $version parcours-integration
 #helm package --version $version referentiel-personnes-api
 
 helm repo index . --url https://softeamouest.github.io/charts/
