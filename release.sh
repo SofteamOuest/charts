eb4fbb5b9a07315fdec32f14830abf87b26ac65c
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2018-09-05 16:41:41 +0200

    commit

--- release.sh
+++ release.sh
@@ -1,13 +1,13 @@
-version=0.1.29
+version=0.1.30
 
 #helm package --version $version api-gateway
 #helm package --version $version evenement-parcours-integration
 #helm package --version $version evenement-rappel
-helm package --version $version gestion-evenement
+#helm package --version $version gestion-evenement
 #helm package --version $version gestion-images
 #helm package --version $version gestion-personnes
 #helm package --version $version parcours-integration
-#helm package --version $version referentiel-personnes-api
+helm package --version $version referentiel-personnes-api
 #helm package --version $version referentiel-personnes-ui
 #helm package --version $version grav
 
