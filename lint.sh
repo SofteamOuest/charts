23b84ba3ae766b99db3bec42607137f517aa080c
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2018-09-13 10:17:15 +0200

    commit

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 
 helm lint api-gateway
 helm lint evenement-parcours-integration
@@ -8,3 +8,4 @@ helm lint gestion-personnes
 helm lint parcours-integration
 helm lint referentiel-personnes-mock
 helm lint referentiel-personnes-api
+helm lint skills-api
--- release.sh
+++ release.sh
@@ -1,16 +1,17 @@
-version=0.1.45
+version=0.1.46
 
-helm package --version $version api-gateway
-helm package --version $version evenement-parcours-integration
-helm package --version $version evenement-rappel
-helm package --version $version gestion-evenement
-helm package --version $version gestion-images
-helm package --version $version gestion-personnes
-helm package --version $version parcours-integration
-helm package --version $version referentiel-personnes-api
-helm package --version $version referentiel-personnes-mock
-helm package --version $version referentiel-personnes-ui
-helm package --version $version grav
+#helm package --version $version api-gateway
+#helm package --version $version evenement-parcours-integration
+#helm package --version $version evenement-rappel
+#helm package --version $version gestion-evenement
+#helm package --version $version gestion-images
+#helm package --version $version gestion-personnes
+#helm package --version $version parcours-integration
+#helm package --version $version referentiel-personnes-api
+#helm package --version $version referentiel-personnes-mock
+#helm package --version $version referentiel-personnes-ui
+#helm package --version $version grav
+helm package --version $version skills-api
 
 helm repo index . --url https://softeamouest.github.io/charts/
 
